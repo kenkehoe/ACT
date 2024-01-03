470e706b50593bc6dc472fdb0d5b148de98009e8
kenkehoe <kenkehoe@users.noreply.github.com>
2024-01-03 21:57:02 +0000

    deploy: 865675480ae36b1ba40ba5923a0f15fd7e166a95

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g0025a74',
+    VERSION: '0.post1+g8656754',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
