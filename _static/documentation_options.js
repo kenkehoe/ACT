06d1693e6ba1cfe22b92624e687237be352ce04a
kenkehoe <kenkehoe@users.noreply.github.com>
2024-01-26 21:37:32 +0000

    deploy: cb21eb1d919bec68a22dda62cbe4e78b96c0d440

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,6 @@
-const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.post1+g6d4e594',
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.post1+gcb21eb1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
